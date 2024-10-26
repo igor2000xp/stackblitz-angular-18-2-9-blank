3f696b331cddee24cdd05c643e6264713790ce07
igor2000xp <igorring@gmail.com>
2024-10-26 09:11:26 +0200

    feat: switchMap() show

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from "@angular/core";
+import {
+  Observable,
+  Subscription,
+  concatMap,
+  delay,
+  map,
+  mergeMap,
+  switchMap,
+  tap,
+  timeout,
+  timer,
+} from "rxjs";
 
 @Component({
-  selector: 'app-home',
+  selector: "app-home",
   standalone: true,
   imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  templateUrl: "./home.component.html",
+  styleUrl: "./home.component.css",
 })
-export class HomeComponent {
+export class HomeComponent implements OnDestroy {
+  marbleObservable$ = new Observable<string>((subscriber) => {
+    subscriber.next("A");
+    setTimeout(() => {
+      subscriber.next("B");
+    }, 100);
+    setTimeout(() => {
+      subscriber.next("C");
+    }, 100);
+  });
 
+  marbleObservableChild$ = new Observable<string>((subscriber) => {
+    setTimeout(() => {
+      subscriber.next("1");
+    }, 2000);
+    setTimeout(() => {
+      subscriber.next("2");
+    }, 3000);
+    setTimeout(() => {
+      subscriber.next("3");
+    }, 4000);
+  });
+  count = 0;
+  m = "";
+
+  sub = new Subscription();
+
+  constructor() {
+    this.marbleObservable$
+      .pipe(
+        mergeMap((parentValue) => {
+          return this.marbleObservableChild$.pipe(
+            map((childValue) => parentValue + childValue) // Concatenate parent and child values
+          );
+        })
+      )
+      .subscribe((child) => console.log(child));
+  }
+  ngOnDestroy(): void {
+    // throw new Error("Method not implemented.");
+    this.sub.unsubscribe();
+  }
 }
